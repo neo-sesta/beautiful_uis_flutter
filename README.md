c6fc0b58e03f68e35c6dcf9deb08bb2ae4ae3fb7
neo-sesta <shuhei.takeda@zozo.com>
2018-08-21 15:59:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# beautiful_uis_flutter
+
+Building Beautiful UIs with Flutter
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() { runApp(new FriendlyChatApp()); }
+
+class FriendlyChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Friendlychat",
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendlychat")),
+      body: _buildTextComposer(),
+    );
+  }
+  
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(hintText: 'Send a message'),
+      )
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+}
